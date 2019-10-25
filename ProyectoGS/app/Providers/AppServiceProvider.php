bc24005853a147d8af4304a73a05b614cc870675
Ricardo Uribia <ricardomanueluribia@gmail.com>
2019-10-25 10:45:06 -0500

    creacion copntroller y tabla empleados

--- ProyectoGS/app/Providers/AppServiceProvider.php
+++ ProyectoGS/app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-         if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-            Schema::defaultStringLength(191);
-        }
+
         
     }
 
