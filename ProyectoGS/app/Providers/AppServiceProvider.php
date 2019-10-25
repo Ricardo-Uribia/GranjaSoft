08b3b733d4af9fb4b1ea0b8b35466a6e5d43df7c
Ricardo Uribia <ricardomanueluribia@gmail.com>
2019-10-25 09:41:40 -0500

    service providers

--- ProyectoGS/app/Providers/AppServiceProvider.php
+++ ProyectoGS/app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+         if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+            Schema::defaultStringLength(191);
+        }
+        
     }
 
     /**
